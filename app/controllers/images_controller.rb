e269e6a5e6f4c64b2cec76e88df15d97badae78f
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-01 11:24:49 +0200

    fixed critical bug in hashtag system that was brekaing image upload, second attempt

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -31,63 +31,64 @@ class ImagesController < ApplicationController
 
   def edit
     @image = Image.find(params[:id])
+  end
 
-    def update
-      @image = Image.find(params[:id])
-      @image.update(image_params)
-      hashtags @image.caption
-      redirect_to user_image_path(current_user,@image)
+  def update
+    @image = Image.find(params[:id])
+    @image.update(image_params)
+    hashtags @image.caption
+    redirect_to user_image_path(current_user,@image)
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to feed_path(current_user)
+
+
+  end
+
+  def hash
+    @hashtagText = params[:hash]
+    hashtag = Hashtag.find_by(hashtag: @hashtagText)
+    @images = hashtag.images
+
+  end
+
+  def image_params
+    params.require(:image).permit(:caption, :public, :image)
+  end
+
+  def hashtags(caption)
+    letterArr = caption.split("")
+    hashFound = false
+    hash = ""
+    letterArr.each_with_index do |letter, index|
+      if hashFound && letter!=" " && letter!="#"
+        hash << letter
+
+      end
+      if hashFound && (letter == " "||letter=="#"||index==(letterArr.length-1))
+        hashFound = false
+        if Hashtag.where(hashtag: hash).count == 0
+
+          @hashtag = @image.hashtags.new(hashtag: hash)
+          @hashtag.save
+        else
+          @hashtag = Hashtag.where(hashtag: hash).first
+          if !@image.hashtags.include?(@hashtag)
+            @image.hashtags << (@hashtag)
+          end
+        end
+
+        hash = ""
+      end
+      if letter == "#"
+        hashFound = true
+      end
     end
 
-    def destroy
-      @image = Image.find(params[:id])
-      @image.destroy
-      redirect_to feed_path(current_user)
-
-
-    end
-
-    def hash
-      @hashtagText = params[:hash]
-      hashtag = Hashtag.find_by(hashtag: @hashtagText)
-      @images = hashtag.images
-
-    end
-
-    def image_params
-      params.require(:image).permit(:caption, :public, :image)
-    end
-
-    def hashtags(caption)
-	      letterArr = caption.split("")
-	      hashFound = false
-	      hash = ""
-	      letterArr.each_with_index do |letter, index|
-		        if hashFound && letter!=" " && letter!="#"
-		          	hash << letter
-
-		        end
-		        if hashFound && (letter == " "||letter=="#"||index==(letterArr.length-1))
-			          hashFound = false
-			          if Hashtag.where(hashtag: hash).count == 0
-
-				            @hashtag = @image.hashtags.new(hashtag: hash)
-				            @hashtag.save
-			          else
-				            @hashtag = Hashtag.where(hashtag: hash).first
-				            if !@image.hashtags.include?(@hashtag)
-				              	@image.hashtags << (@hashtag)
-				            end
-			          end
-
-			          hash = ""
-		        end
-		        if letter == "#"
-		          	hashFound = true
-		        end
-	      end
-    end
 
 
 
- end
+  end
