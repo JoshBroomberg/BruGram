9ab7e53c0d1409915041c1792d7b791ff2b287b5
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-02 10:50:59 +0200

    Implemented new bootstrap theme v1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require wow.min
 //= require twitter/bootstrap
+//= require touchSwipe.min
+//= require respond
+//= require jquery.blueimp-gallery.min
+//= require script
 //= require turbolinks
 //= require_tree .
